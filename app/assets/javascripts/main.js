2566da72c1aa35dc783cbf7173f3059506317c44
Theodore Cleveland <tcclevela@gmail.com>
2013-08-26 13:16:10 -0400

    restructure into rails app

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,6 @@
+$('a').click(function(){
+  $('html, body').animate({
+    scrollTop: $( $(this).attr('href') ).offset().top
+  }, 1000);
+  return false;
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wdi6::Application.config.secret_key_base = '9840bffddc068fc566ddd4fb7d3cce51f40b19082624c7e42c4da82d9c63000e759ae17199239f2d4e3364428b6f2519e5337fd5523cdc1cab5576ad6fc05c4d'
